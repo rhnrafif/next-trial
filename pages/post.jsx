92c41bd98de83b0b08bc3bdc98b248148474e6ea
raihanudin rafif <raihanudinrafif@gmail.com>
2022-12-22 20:42:19 +0700

    fisrt initial commit

---
+++ pages/post.jsx
@@ -0,0 +1,7 @@
+export default function Postingan(){
+    return(
+        <main>
+        <h1 className='text-2xl text-center font-bold text-sky-500 mt-20'>Post from next JS</h1>
+      </main>
+    )
+}
